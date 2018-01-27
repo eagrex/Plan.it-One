7793a9974426a23937570c160d8eb4c3ba3c2159
Yi <eagrex@gmail.com>
2018-01-27 14:14:05 +0800

    Convert to MVC Design Pattern.

--- Plan.it One/Controllers/PlanitViewController.swift
+++ Plan.it One/Controllers/PlanitViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 
 class PlanitViewController: UITableViewController {
 
-    var itemArray = ["Find Mike", "Buy Eggs", "Destroy Demon"]
+    var itemArray = [Item]()
     
-    var defaults = UserDefaults.standard
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "PlanitListArray") as? [String] {
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Save Him"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Kill Him"
+        itemArray.append(newItem3)
+        
+        if let items = defaults.array(forKey: "PlanitListArray") as? [Item] {
             itemArray = items
         }
 
@@ -32,7 +44,11 @@ class PlanitViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "PlanitItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
@@ -42,12 +58,9 @@ class PlanitViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -62,7 +75,10 @@ class PlanitViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.setValue(self.itemArray, forKey: "PlanitListArray")
             
---
+++ Plan.it One/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Plan.it One
+//
+//  Created by Yi Wang on 27/01/2018.
+//  Copyright © 2018 yi. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
